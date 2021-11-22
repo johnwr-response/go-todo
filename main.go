b1f77d3d7802440b5cf728baae00efae1d3b5a7d
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2021-11-22 23:04:56 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/johnwr-response/go-todo/database"
+	"github.com/johnwr-response/go-todo/todo"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(cors.New())
+	database.ConnectDB()
+	defer database.DB.Close()
+
+	api := app.Group("/api")
+	todo.Register(api, database.DB)
+
+	log.Fatal(app.Listen(":5000"))
+}
